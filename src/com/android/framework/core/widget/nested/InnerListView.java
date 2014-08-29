3d13b6e5f9a88bfc64755b3e5df17b102f7bb55c
luohong722 <luohong722@yahoo.cn>
2014-08-29 16:44:04 +0800

    添加嵌套ListView和ScrollView

---
+++ src/com/android/framework/core/widget/nested/InnerListView.java
@@ -0,0 +1,68 @@
+
+package com.android.framework.core.widget.nested;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ListView;
+import android.widget.ScrollView;
+
+public class InnerListView extends ListView {
+
+	private int maxHeight;
+
+	private ScrollView parentScrollView;
+
+	public ScrollView getParentScrollView() {
+		return parentScrollView;
+	}
+
+	public void setParentScrollView(ScrollView parentScrollView) {
+		this.parentScrollView = parentScrollView;
+	}
+
+	public int getMaxHeight() {
+		return maxHeight;
+	}
+
+	public void setMaxHeight(int maxHeight) {
+		this.maxHeight = maxHeight;
+	}
+
+	public InnerListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		if (maxHeight > -1) {
+			heightMeasureSpec = MeasureSpec.makeMeasureSpec(maxHeight, MeasureSpec.AT_MOST);
+		}
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		switch (ev.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			setParentScrollAble(false);
+		case MotionEvent.ACTION_MOVE:
+			break;
+		case MotionEvent.ACTION_UP:
+		case MotionEvent.ACTION_CANCEL:
+			setParentScrollAble(true);
+			break;
+		default:
+			break;
+		}
+		return super.onInterceptTouchEvent(ev);
+	}
+
+	/**
+	 * @param flag
+	 */
+	private void setParentScrollAble(boolean flag) {
+		parentScrollView.requestDisallowInterceptTouchEvent(!flag);
+	}
+
+}
